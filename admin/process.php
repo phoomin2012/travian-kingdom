b08303ed334e762b9c0fa38a066670659cec6390
phoomin2012 <phoomin009@gmail.com>
2017-09-02 00:41:00 +0700

    fix

--- admin/process.php
+++ admin/process.php
@@ -6,40 +6,36 @@ if (isset($_GET['id'])) {
     $s = $engine->database->getServer($_GET['id']);
     $engine->server = (object) $s;
 }
-if ($s) {
-    switch ($_GET['p']) {
-        case 'install':
-            if ($s['installed'] == "1") {
-                $q = query("SHOW TABLES LIKE ?;", ["%{$s['prefix']}%"])->fetchAll();
-                foreach ($q as $n) {
-                    $n = $n[0];
-                    query("TRUNCATE TABLE `{$n}`");
-                }
-                $engine->world->generateMap();
-                query("UPDATE `global_server_data` SET `start`=? WHERE `sid`=?;", [time(), $_GET['id']]);
-            } else {
-                
-            }
-            header("Location: index.php?p=info&id={$_GET['id']}");
-            break;
-        case 'import':
-            $sql = file_get_contents(__DIR__ . '/sql.sql');
-            query($sql);
+switch ($_GET['p']) {
+    case 'install':
+        if ($s['installed'] == "1") {
             $q = query("SHOW TABLES LIKE ?;", ["%{$s['prefix']}%"])->fetchAll();
             foreach ($q as $n) {
                 $n = $n[0];
                 query("TRUNCATE TABLE `{$n}`");
             }
-            $tbs = ['error_nodejs', 'error_php', 'global_avatar', 'global_msid', 'global_user', 'process_php'];
-            foreach ($tbs as $tb) {
-                query("TRUNCATE TABLE `{$tb}`");
-            }
-            header("Location: index.php");
-            break;
-        default:
-            header("Location: index.php");
-            break;
-    }
-} else {
-    header("Location: index.php");
+            $engine->world->generateMap();
+            query("UPDATE `global_server_data` SET `start`=? WHERE `sid`=?;", [time(), $_GET['id']]);
+        } else {
+            
+        }
+        header("Location: index.php?p=info&id={$_GET['id']}");
+        break;
+    case 'import':
+        $sql = file_get_contents(__DIR__ . '/sql.sql');
+        query($sql);
+        $q = query("SHOW TABLES LIKE ?;", ["%{$s['prefix']}%"])->fetchAll();
+        foreach ($q as $n) {
+            $n = $n[0];
+            query("TRUNCATE TABLE `{$n}`");
+        }
+        $tbs = ['error_nodejs', 'error_php', 'global_avatar', 'global_msid', 'global_user', 'process_php'];
+        foreach ($tbs as $tb) {
+            query("TRUNCATE TABLE `{$tb}`");
+        }
+        header("Location: index.php");
+        break;
+    default:
+        header("Location: index.php");
+        break;
 }
